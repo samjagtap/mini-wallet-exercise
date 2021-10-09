95b627afc82e74d03b8e82cd15d2038ab9e1530e
Sambhaji Jagtap <sambhaji.jagtap@beautosys.com>
2021-10-09 13:43:25 +0530

    Update script

--- scripts/setup_customer.py
+++ scripts/setup_customer.py
@@ -2,17 +2,15 @@ from django.contrib.auth.models import User
 from exercise.wallets.models import Customer
 
 
-def run_script():
+def run():
+    print("Initializing the mini-wallet-exercise app by creating necessary things.")
     if not User.objects.filter(username='test').first():
         user = User.objects.create(username='test', email='example@test.com')
         customer = Customer.objects.create(customer_profile=user)
         customer_xid = customer.customer_xid
-        print("Your customer_xid is {}".format(customer_xid))
+        print("Setup complete. Your customer_xid is {}".format(customer_xid))
     else:
         user = User.objects.filter(username='test').first()
         customer = Customer.objects.filter(customer_profile=user).first()
         customer_xid = customer.customer_xid
-        print("Your customer_xid is {}".format(customer_xid))
-
-
-run_script()
+        print("Setup complete. Your customer_xid is {}".format(customer_xid))
